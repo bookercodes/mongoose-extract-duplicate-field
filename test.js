1be53dcb071b663e49f395ed508de66e2e3a4439
Alex Booker <alexbooker@fastmail.im>
2015-07-11 20:22:39 +0100

    normalized quotation marks

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 'use strict';
 var sut = require('./');
-var chai   = require("chai");
+var chai   = require('chai');
 var should = chai.should();
 
 it('should return correct field name', function () {
   var error = {
-    "name": "MongoError",
-    "message": "insertDocument :: caused by :: 11000 E11000 duplicate key error index: authentication.users.$username_1  dup key: { : \"alex\" }",
-    "index": 0,
-    "code": 11000,
-    "errmsg": "insertDocument :: caused by :: 11000 E11000 duplicate key error index: authentication.users.$username_1  dup key: { : \"alex\" }"
+    'name': 'MongoError',
+    'message': 'insertDocument :: caused by :: 11000 E11000 duplicate key error index: authentication.users.$username_1  dup key: { : \'alex\' }',
+    'index': 0,
+    'code': 11000,
+    'errmsg': 'insertDocument :: caused by :: 11000 E11000 duplicate key error index: authentication.users.$username_1  dup key: { : \'alex\' }'
   };
   var key = sut(error);
   key.should.equal('username');
