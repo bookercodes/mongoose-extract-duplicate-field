61231109cdfcd5012cb965b5b4dc672e211b4fd4
Alex Booker <alexbooker@fastmail.im>
2015-07-11 20:23:59 +0100

    normalized quotation mark

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 module.exports = function (error) {
   var field = error.message.split('.$')[1];
   field = field.split(' dup key')[0];
-  field = field.substring(0, field.lastIndexOf("_"));
+  field = field.substring(0, field.lastIndexOf('_'));
   return field;
 };
